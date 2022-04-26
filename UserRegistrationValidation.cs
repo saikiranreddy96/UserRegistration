47b455bbb8ee5f1f68aff3bd8fc2ea8992944565
saikiranreddy <saikiranreddy@gmail.com>
2022-04-27 01:35:37 +0530

    [kiran]-Added Validate First Name

---
+++ UserRegistrationValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class UserRegistrationValidation
+    {
+        //UC1-validate first name
+        public static void ValidateFirstName()
+        {
+            Console.WriteLine("Please Enter your First Name:");
+            string firstName = Console.ReadLine();
+            string firstName_pattern = "^[A-Z][a-z]{2,}$";
+
+            if (Regex.IsMatch(firstName, firstName_pattern))
+            {
+                Console.WriteLine("First Name is Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Valid");
+            }
+        }
+    }
+}
